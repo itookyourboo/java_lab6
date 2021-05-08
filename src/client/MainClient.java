1e943e9c0c1c6841dae9881a87d136f536b44230
itookyourboo <Wignorbo@yandex.ru>
2021-05-08 23:07:44 +0300

    Main window with table

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -1,13 +1,29 @@
 package client;
 
+import client.gui.LoginWindow;
+import client.gui.MainWindow;
+import client.impl.LoginContract;
 import client.util.CommandManager;
 import client.util.Interactor;
 import common.Config;
+import common.Crypto;
+import common.model.StudyGroup;
+import common.model.User;
+import common.net.CommandResult;
+import common.net.Request;
+import common.net.ResultStatus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class MainClient {
     private static int port = Config.PORT;
+    private static RequestSender requestSender;
+    private static CommandManager commandManager;
+    private static Scanner scanner;
 
     public static void main(String[] args) {
         if (args.length == 1) {
@@ -17,21 +33,61 @@ public class MainClient {
                 System.out.println("Не получается спарсить порт.");
             }
         }
+        connect(port);
+        showLoginWindow();
+    }
 
-        RequestSender requestSender = new RequestSender(port);
-        Scanner scanner = new Scanner(System.in);
-        CommandManager commandManager = new CommandManager(requestSender, scanner);
-        System.out.println("Клиент запущен! Порт: " + port);
-        String input;
-        do {
-            if (!scanner.hasNextLine()) return;
-            input = scanner.nextLine();
-            try {
-                commandManager.parseAndExecute(input);
-            } catch (Exception e) {
-                Interactor.printError(e.getMessage());
+    private static void showLoginWindow() {
+        LoginWindow loginWindow = new LoginWindow();
+        loginWindow.setLoginContract(new LoginContract() {
+            @Override
+            public void onLoginClicked(String username, String password) {
+                User user = new User(username, Crypto.encryptString(password));
+                Request<User> request = new Request<>("login", user);
+                CommandResult result = requestSender.sendRequest(request);
+                if (result.status == ResultStatus.OK) {
+                    showMainWindow();
+                    loginWindow.setVisible(false);
+                    requestSender.setUser(user);
+                } else
+                    loginWindow.showError(result.message);
             }
-            Interactor.println("");
-        } while (!input.equals("exit"));
+
+            @Override
+            public void onRegisterClicked(String username, String password) {
+                User user = new User(username, Crypto.encryptString(password));
+                Request<User> request = new Request<>("register", user);
+                CommandResult result = requestSender.sendRequest(request);
+                if (result.status == ResultStatus.OK) {
+                    showMainWindow();
+                    loginWindow.setVisible(false);
+                    requestSender.setUser(user);
+                } else
+                    loginWindow.showError(result.message);
+            }
+        });
+        loginWindow.setVisible(true);
+    }
+
+    private static void showMainWindow() {
+        List<StudyGroup> list;
+        Request<String> request = new Request<>("show", null);
+        CommandResult result = requestSender.sendRequest(request);
+        if (result.status == ResultStatus.OK) {
+            list = Arrays.stream(result.message.split("\n"))
+                    .map(StudyGroup::fromJson)
+                    .collect(Collectors.toList());
+        } else {
+            list = new ArrayList<>();
+        }
+        MainWindow mainWindow = new MainWindow(list);
+        mainWindow.setVisible(true);
+    }
+
+    private static void connect(int port) {
+        requestSender = new RequestSender(port);
+        scanner = new Scanner(System.in);
+        commandManager = new CommandManager(requestSender, scanner);
+        System.out.println("Клиент запущен! Порт: " + port);
     }
 }
---
+++ src/client/gui/MainWindow.java
@@ -0,0 +1,162 @@
+package client.gui;
+
+import client.impl.OnConnectButtonClickedListener;
+import client.util.LocaleManager;
+import common.Config;
+import common.model.StudyGroup;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class MainWindow extends JFrame {
+
+    private final List<StudyGroup> studyGroups;
+
+    public MainWindow(List<StudyGroup> studyGroups) {
+        super(LocaleManager.getString("loginTitle"));
+        this.studyGroups = studyGroups;
+        initWindow();
+        initComponents();
+    }
+
+    private void initWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize().getSize();
+        int width = dimension.width, height = dimension.height;
+        int WINDOW_WIDTH = width / 2, WINDOW_HEIGHT = height / 2;
+        setBounds(width / 2 - WINDOW_WIDTH / 2, height / 2 - WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT);
+//        setResizable(false);
+    }
+
+    private void initComponents() {
+        setLayout(new BorderLayout());
+        Object[][] data = new Object[studyGroups.size()][];
+        int i = 0;
+        for (StudyGroup studyGroup: studyGroups) {
+            data[i++] = studyGroupToObjectArray(studyGroup);
+        }
+
+        JTable jTable = new JTable(data, columns);
+        jTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        jTable.setAutoCreateRowSorter(true);
+        //jTable.setRowSorter(new TableRowSorter());
+        add(jTable.getTableHeader(), BorderLayout.NORTH);
+        add(jTable, BorderLayout.CENTER);
+    }
+
+    String[] columns = new String[] {
+            "ID",
+            "Name",
+            "X",
+            "Y",
+            "Creation date",
+            "Students count",
+            "Expelled students",
+            "Should be expelled",
+            "Form of education",
+            "Admin",
+            "Admin's weight",
+            "Admin's passport",
+            "Admin's X",
+            "Admin's Y",
+            "Admin's location"
+    };
+
+    private Object[] studyGroupToObjectArray(StudyGroup studyGroup) {
+        return new Object[] {
+                studyGroup.getId(),
+                studyGroup.getName(),
+                studyGroup.getCoordinates().getX(),
+                studyGroup.getCoordinates().getY(),
+                studyGroup.getCreationDate(),
+                studyGroup.getStudentsCount(),
+                studyGroup.getExpelledStudents(),
+                studyGroup.getShouldBeExpelled(),
+                studyGroup.getFormOfEducation(),
+                studyGroup.getGroupAdmin().getName(),
+                studyGroup.getGroupAdmin().getWeight(),
+                studyGroup.getGroupAdmin().getPassportID(),
+                studyGroup.getGroupAdmin().getLocation().getX(),
+                studyGroup.getGroupAdmin().getLocation().getY(),
+                studyGroup.getGroupAdmin().getLocation().getName()
+        };
+    }
+
+    class TableRowSorter extends RowSorter<TableModel> {
+
+        @Override
+        public TableModel getModel() {
+            return null;
+        }
+
+        @Override
+        public void toggleSortOrder(int column) {
+
+        }
+
+        @Override
+        public int convertRowIndexToModel(int index) {
+            return 0;
+        }
+
+        @Override
+        public int convertRowIndexToView(int index) {
+            return 0;
+        }
+
+        @Override
+        public void setSortKeys(List<? extends SortKey> keys) {
+
+        }
+
+        @Override
+        public List<? extends SortKey> getSortKeys() {
+            return null;
+        }
+
+        @Override
+        public int getViewRowCount() {
+            return 0;
+        }
+
+        @Override
+        public int getModelRowCount() {
+            return 0;
+        }
+
+        @Override
+        public void modelStructureChanged() {
+
+        }
+
+        @Override
+        public void allRowsChanged() {
+
+        }
+
+        @Override
+        public void rowsInserted(int firstRow, int endRow) {
+
+        }
+
+        @Override
+        public void rowsDeleted(int firstRow, int endRow) {
+
+        }
+
+        @Override
+        public void rowsUpdated(int firstRow, int endRow) {
+
+        }
+
+        @Override
+        public void rowsUpdated(int firstRow, int endRow, int column) {
+
+        }
+    }
+}
